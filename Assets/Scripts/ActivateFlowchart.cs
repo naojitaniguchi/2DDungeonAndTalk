05bcab98329cc69f40397797a2e3f8ed1a0d415d
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-09-30 11:18:25 +0900

    Added ActivateFlochart.cs .

---
+++ Assets/Scripts/ActivateFlowchart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFlowchart : MonoBehaviour
+{
+    public float effectTime = 2.0f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ActivateTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ActivateTarget()
+    {
+        yield return new WaitForSeconds(effectTime);
+
+        target.SetActive(true);
+    }
+}
